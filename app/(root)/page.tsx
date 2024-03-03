9701adf4629341119d6f4ba77810cc25d445fb1b
aziz0519 <aziz2834@gmail.com>
2024-03-03 11:29:08 +0800

    updated actions

---
+++ app/(root)/page.tsx
@@ -0,0 +1,35 @@
+import { navLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <>
+    <section className="home">
+      <h1 className="home-heading">
+        Unleash Your Creative Vision!
+      </h1>
+      <ul className="flex-center w-full gap-20">
+        {navLinks.map((link) => (
+          <Link
+          key={link.route}
+          href={link.route}
+          className="flex-center flex-col gap-2" 
+          >
+            <li className="flex-center w-fit rounded-full bg-white p-4">
+              <Image src={link.icon} alt='image' width={24} height={24} />
+            </li>
+            <p className="p-14-medium text-center text-white">{link.label}</p>
+          </Link>
+        ))}
+      </ul>
+    </section>
+    <section className="sm:mt-12">
+      
+
+    </section>
+    </>
+  )
+}
+
+export default Home;
